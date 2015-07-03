29ff6738403f6625caa427ceffcd5e0b2b8c190a
Luke Skibinski <l.skibinski@elifesciences.org>
2015-07-03 11:39:39 +0100

    added a test to ensure the data of the article being imported is the same as what comes out

--- src/publisher/tests/test_json_import.py
+++ src/publisher/tests/test_json_import.py
@@ -3,6 +3,10 @@ from publisher import json_import as ingest, utils, models, logic
 
 from base import BaseCase
 
+import logging
+
+logging.getLogger("").setLevel(logging.WARNING) # suppresses debug, info messages
+
 class ImportArticleFromJSON(BaseCase):
     def setUp(self):
         self.journal = logic.journal()
@@ -19,3 +23,14 @@ class ImportArticleFromJSON(BaseCase):
         self.assertEqual(0, models.Article.objects.count())
         ingest.import_article(self.journal, self.json_fixture)
         self.assertEqual(1, models.Article.objects.count())
+
+    def test_article_data(self):
+        expected_data = {
+            'title':  "Molecular architecture of human polycomb repressive complex 2",
+            'version': 1,
+            'doi': "10.7554/eLife.00005",
+            'journal': self.journal,
+        }
+        article = ingest.import_article(self.journal, self.json_fixture)
+        for attr, expected_value in expected_data.items():
+            self.assertEqual(getattr(article, attr), expected_value)
