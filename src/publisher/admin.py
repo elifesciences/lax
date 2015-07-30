a6e6c0b357c20b906ac02a6ebc819d6581eb61cb
Luke Skibinski <l.skibinski@elifesciences.org>
2015-07-30 17:12:06 +0100

    added improved model history to supported models for admin

--- src/publisher/admin.py
+++ src/publisher/admin.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
 import models
+from simple_history.admin import SimpleHistoryAdmin
+
+class ArticleAdmin(SimpleHistoryAdmin):
+    pass
+
+class ArticleAttributeAdmin(SimpleHistoryAdmin):
+    pass
 
 admin_list = [
     (models.Publisher,),
-    (models.Journal,),
-    (models.Article,) ,
+    (models.Journal, ),
+    (models.Article, ArticleAdmin),
+    (models.ArticleAttribute, ArticleAttributeAdmin),
 ]
 
 [admin.site.register(*t) for t in admin_list]
