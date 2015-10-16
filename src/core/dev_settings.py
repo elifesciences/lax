18315e2de55cdbee6b66ee1afd7dfbb45e46d2f1
Luke Skibinski <l.skibinski@elifesciences.org>
2015-10-16 17:21:59 +0100

    disabled any sort of authentication rest framework is trying to do.

--- src/core/dev_settings.py
+++ src/core/dev_settings.py
@@ -121,7 +121,9 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
     #'DEFAULT_PERMISSION_CLASSES': [
     #    'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    #]
+    #],
+    'DEFAULT_AUTHENTICATION_CLASSES': [],
+    'DEFAULT_PERMISSION_CLASSES': [],
 }
 
 SWAGGER_SETTINGS = {
