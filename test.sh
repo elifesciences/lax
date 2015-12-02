8b11a55ee82c13322889c9b2d040b1866a493fa1
Luke Skibinski <l.skibinski@elifesciences.org>
2015-12-02 10:49:20 +0000

    added pylint review to testing but disabled it until the upstream is less broken

--- test.sh
+++ test.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 source install.sh > /dev/null
+# enable once pylint+pylint-django are working again
+#pylint -E ./src/publisher/** --load-plugins=pylint_django --disable=E1103
 ./src/manage.py test src/
