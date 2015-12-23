61acad6460994f1524be2a8d060b759d4cc13842
Luke Skibinski <l.skibinski@elifesciences.org>
2015-12-23 16:00:09 +0000

    re-enabled pylint - appears to be working now

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 source install.sh > /dev/null
 # enable once pylint+pylint-django are working again
-#pylint -E ./src/publisher/** --load-plugins=pylint_django --disable=E1103
+pylint -E ./src/publisher/** --load-plugins=pylint_django --disable=E1103
+echo "passed linting"
 ./src/manage.py test src/
