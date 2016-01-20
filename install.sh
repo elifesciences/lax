afe1c0f746bf416c475a0239bba92ffc8f82f1c7
Luke Skibinski <l.skibinski@elifesciences.org>
2016-01-20 12:07:56 +0000

    removed old comment from install script

--- install.sh
+++ install.sh
@@ -5,8 +5,6 @@ if [ ! -d venv ]; then
 fi
 source venv/bin/activate
 if [ ! -f src/core/settings.py ]; then
-    #echo "no settings.py found. if this is a dev environment you can do 'ln -s dev_settings.py settings.py' in the src/core/ directory. Quitting."
-    #exit 1
     echo "no settings.py found! using the DEV settings (dev_settings.py) by default."
     cd src/core/
     ln -s dev_settings.py settings.py
