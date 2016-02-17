0a982cfb77e42749c7793ea67a409ae4df6a7b88
Luke Skibinski <l.skibinski@elifesciences.org>
2016-02-17 16:29:05 +0000

    updated test script

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-source install.sh > /dev/null
-# enable once pylint+pylint-django are working again
+set -e
+source install.sh
 pylint -E ./src/publisher/** --load-plugins=pylint_django --disable=E1103
 echo "* passed linting"
 coverage run --source='src/' src/manage.py test src/
