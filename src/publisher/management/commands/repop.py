b4c1ad1eadd8fae65aa8d06f13bbf8c6610894e8
Luke Skibinski <l.skibinski@elifesciences.org>
2016-02-19 11:07:27 +0000

    repub, eif is no longer downloaded from the bucket if the file exists on disk.

--- src/publisher/management/commands/repop.py
+++ src/publisher/management/commands/repop.py
@@ -1,10 +1,19 @@
 import os
 from pprint import pprint
 from django.core.management.base import BaseCommand
+from django.core.management import call_command
 import boto3
 
 import logging
 logger = logging.getLogger(__name__)
+
+def mkstruct(o):
+    keys = ['key', 'last_modified']
+    struct = dict(map(lambda attr: (attr, getattr(o, attr)), keys))
+    struct.update(dict(zip(['aid', 'uuid', 'fname'], struct['key'].split('/'))))
+    struct['last_modified'] = struct['last_modified'].strftime('%Y-%m-%d-%H-%M-%S')
+    #struct['obj'] = o
+    return struct
 
 class Command(BaseCommand):
     help = '''Repopulates the lax database using the contents of the elife-publishing-eif bucket. If article has multiple attempts, use the most recent attempt. Matching articles will be downloaded and imported using the `import_articles` command'''
@@ -14,16 +23,8 @@ class Command(BaseCommand):
         bucket = s3.Bucket('elife-publishing-eif')
         q = bucket.objects.all()
         #q = bucket.objects.limit(count=10)
-        #res = [r for r in q]
-        def _struct(o):
-            keys = ['key', 'last_modified']
-            struct = dict(map(lambda attr: (attr, getattr(o, attr)), keys))
-            struct.update(dict(zip(['aid', 'uuid', 'fname'], struct['key'].split('/'))))
-            struct['last_modified'] = struct['last_modified'].strftime('%Y-%m-%d-%H-%M-%S')
-            #struct['obj'] = o
-            return struct
         # turn those objects into something we can deal with
-        dataset = map(_struct, q)
+        dataset = map(mkstruct, q)
         # order everything by when they were modified, from earliest to oldest
         dataset = sorted(dataset, key=lambda x: x['last_modified'])
         # key by fname so more recent runs of the same file override previous runs
@@ -42,7 +43,14 @@ class Command(BaseCommand):
         # download everything
         os.system('mkdir -p .repop')
         def download(s):
-            print 'downloading',s['key']
-            bucket.download_file(s['key'], '.repop/%s' % s['fname'])
+            path = '.repop/%s' % s['fname']
+            if os.path.exists(path):
+                print 'file exists, NOT downloading',s['key']
+            else:
+                print 'downloading',s['key']
+                bucket.download_file(s['key'], path)
             return key
         map(download, dataset)
+
+        cmd = "import_article path .repop/ --no-update"
+        call_command(*cmd.split())
