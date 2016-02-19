7f0fefe1267d136addf8aa4955dd2573aa7b007a
Luke Skibinski <l.skibinski@elifesciences.org>
2016-02-19 15:13:12 +0000

    added a wrapper around the repop command that ensures the lax aws permissions exist (provided by the elife-builder or -you-, future user)

---
+++ populate-lax-from-production-runs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+./install.sh
+if [ ! -f aws-perms.sh ]; then
+    echo "an 'aws-perms.sh' file not found."
+    echo "this file contains the environment variables required for lax to talk to AWS"
+    exit 1
+fi
+# put the vars into ENV
+set -a; source aws-perms.sh; set +a
+# call the repopulate command
+./manage.sh repop
