7126c0b8b62a9cf2eda7b84951472c6b37503753
Luke Skibinski <l.skibinski@elifesciences.org>
2016-03-08 17:11:30 +0000

    rss, added a feed for specific articles so they can be watched for changes. this feed includes all versions

--- src/publisher/rss.py
+++ src/publisher/rss.py
@@ -20,13 +20,62 @@ class RSSArticleFeedGenerator(Rss201rev2Feed):
         super(RSSArticleFeedGenerator, self).add_item_elements(handler, item)
         handler.addQuickElement("dc:date", item['pubdate'].isoformat())
 
-class ArticleFeed(Feed):
+class AbstractArticleFeed(Feed):
     feed_type = RSSArticleFeedGenerator
     title = "eLife Article Feeds"
     description = "eLife Article Feed"
 
     def link(self, obj):
-        return reverse('rss-articles', kwargs=obj['original'])
+        raise NotImplementedError()
+
+    def get_object(self, request, *args, **kwargs):
+        raise NotImplementedError()
+
+    def items(self, obj):
+        "returns a list of models.Article-list objects"
+        raise NotImplementedError()
+
+    def item_title(self, item):
+        return item.title
+
+    def item_pubdate(self, item):
+        return item.datetime_published
+
+    def copyright(self):
+        return 'eLife Sciences Publications Ltd'
+
+    #def licence(self):
+    #    # http://www.rssboard.org/creative-commons
+    #    return 'Creative Commons Attribution 4.0'
+
+
+class SpecificArticleFeed(AbstractArticleFeed):
+
+    def get_object(self, request, aid_list):
+        aid_list = aid_list.split(',')
+        print 'aids',aid_list
+        doi_list = map(lambda aid: '10.7554/'+aid, aid_list)
+        print 'doi',doi_list
+        return {'aid_list': aid_list,
+                'doi_list': doi_list}
+
+    def link(self, obj):
+        return reverse('rss-specific-article-list', kwargs={'aid_list': ','.join(obj['aid_list'])})
+
+    def items(self, obj):
+        return models.Article.objects.filter(doi__in=obj['doi_list']).order_by('datetime_record_updated', 'version')
+
+    def item_title(self, item):
+        return u'%s (version %s)' % (item.title, item.version)
+
+
+class RecentArticleFeed(AbstractArticleFeed):
+    feed_type = RSSArticleFeedGenerator
+    title = "eLife Article Feeds"
+    description = "eLife Article Feed"
+
+    def link(self, obj):
+        return reverse('rss-recent-article-list', kwargs=obj['original'])
 
     def get_object(self, request, article_types, since):
         return {
@@ -42,24 +91,14 @@ class ArticleFeed(Feed):
         ]
         return logic.latest_articles(where=where_clauses)
 
-    def item_title(self, item):
-        return item.title
-
-    def item_pubdate(self, item):
-        return item.datetime_published
-
-    def copyright(self):
-        return 'eLife Sciences Publications Ltd'
-
-    #def licence(self):
-    #    # http://www.rssboard.org/creative-commons
-    #    return 'Creative Commons Attribution 4.0'
-
 #
 # rss handling
 #
 
+# rooted at /rss/articles/ in urls.py
 urls = [
-    url("^(?P<article_types>(poa\+vor)|(poa|vor))/last-(?P<since>\d{1,3})-days/$", ArticleFeed(), name='rss-articles'),
+    url(r"^(?P<aid_list>(eLife\.\d{5}[,]?)+)$", SpecificArticleFeed(), name='rss-specific-article-list'),
+    url(r"^(?P<article_types>(poa\+vor)|(poa|vor))/last-(?P<since>\d{1,3})-days/$", RecentArticleFeed(), name='rss-recent-article-list'),
+
 ]
 
--- src/publisher/tests/test_rss.py
+++ src/publisher/tests/test_rss.py
@@ -9,18 +9,13 @@ class RSSViews(BaseCase):
     def setUp(self):
         self.c = Client()
         self.journal = logic.journal()
-
-    def tearDown(self):
-        pass
-
-    def test_last_n_articles(self):
         an_hour_ago = datetime.now() - timedelta(hours=1)
         many_hours_ago = an_hour_ago - timedelta(hours=999)
-        article_data_list = [
+        self.article_data_list = [
             {'title': 'foo',
              'status': 'vor',
              'version': 1,
-             'doi': "10.7554/eLife.DUMMY",
+             'doi': "10.7554/eLife.00001",
              'journal': self.journal,
              'datetime_published': an_hour_ago,
             },
@@ -28,7 +23,7 @@ class RSSViews(BaseCase):
             {'title': 'bar',
              'status': 'vor',
              'version': 1,
-             'doi': "10.7554/eLife.DUMMY1",
+             'doi': "10.7554/eLife.00002",
              'journal': self.journal,
              'datetime_published': many_hours_ago, # **
             },
@@ -36,14 +31,36 @@ class RSSViews(BaseCase):
             {'title': 'baz',
              'version': 1,
              'status': 'poa', # **
-             'doi': "10.7554/eLife.DUMMY2",
+             'doi': "10.7554/eLife.00003",
              'journal': self.journal,
              'datetime_published': an_hour_ago,
              }
         ]
-        [logic.add_or_update_article(**article_data) for article_data in article_data_list]        
-        url = reverse('rss-articles', kwargs={'article_types': 'vor', 'since': '1'})
-        resp = self.c.get(url)
-        print resp.content
-        self.assertEqual(1, len(re.findall('<guid', resp.content)))
+        [logic.add_or_update_article(**article_data) for article_data in self.article_data_list]        
 
+
+    def tearDown(self):
+        pass
+
+    def test_specific_feed_single_article(self):
+        """a specific article can be targeted in the rss. why? spot fixes
+        to ALM and a person may be interested in future versions of a given article"""
+        doi = self.article_data_list[0]['doi']
+        aid = doi[8:]
+        url = reverse('rss-specific-article-list', kwargs={'aid_list': aid})
+        resp = self.c.get(url)
+        self.assertEqual(1, len(re.findall('<guid', resp.content)))
+    
+    def test_specific_feed_many_article(self):
+        """a specific article can be targeted in the rss. why? spot fixes
+        to ALM and a person may be interested in future versions of a given article"""
+        aid_list = map(lambda a: a['doi'][8:], self.article_data_list)
+        aid_str = ','.join(aid_list)
+        url = reverse('rss-specific-article-list', kwargs={'aid_list': aid_str})
+        resp = self.c.get(url)
+        self.assertEqual(3, len(re.findall('<guid', resp.content)))
+
+    def test_last_n_articles(self):
+        url = reverse('rss-recent-article-list', kwargs={'article_types': 'vor', 'since': '1'})
+        resp = self.c.get(url)
+        self.assertEqual(1, len(re.findall('<guid', resp.content)))
