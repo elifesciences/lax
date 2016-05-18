ca16d12c4a8b0502ad67625a787aae296f50ddb4
Giorgio Sironi <g.sironi@elifesciences.org>
2016-05-18 10:46:01 +0000

    Test should not require user input
    Tests should not require user interaction to be fully automated. If the prompt about creating the database comes out, the build immediately fails

--- .test.sh
+++ .test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # called by test.sh
-coverage run --source='src/' --omit='*/tests/*,*/migrations/*' src/manage.py test src/
+coverage run --source='src/' --omit='*/tests/*,*/migrations/*' src/manage.py test src/ --no-input
 echo "* passed tests"
