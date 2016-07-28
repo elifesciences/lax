0751639d13a97463e4b4b58b9e8a235a8cf24354
Luke Skibinski <l.skibinski@elifesciences.org>
2016-07-28 16:07:52 +0100

    we're going to be using manuscript ids as the article identifier, going to assume the non-zero padded version.

--- src/publisher/api_v2_urls.py
+++ src/publisher/api_v2_urls.py
@@ -3,7 +3,7 @@ import api_v2_views as views
 
 urlpatterns = [
     url(r'articles$', views.article_list, name='article-list'),
-    url(r'articles/(?P<id>\w+)$', views.article, name='article'),
-    url(r'articles/(?P<id>\w+)/versions$', views.article_version_list, name='article-version-list'),
-    url(r'articles/(?P<id>\w+)/versions/(?P<version>\d+)$', views.article_version, name='article-version'),
+    url(r'articles/(?P<id>\d+)$', views.article, name='article'),
+    url(r'articles/(?P<id>\d+)/versions$', views.article_version_list, name='article-version-list'),
+    url(r'articles/(?P<id>\d+)/versions/(?P<version>\d+)$', views.article_version, name='article-version'),
 ]
--- src/publisher/tests/test_api_v2_views.py
+++ src/publisher/tests/test_api_v2_views.py
@@ -5,8 +5,8 @@ from django.core.urlresolvers import reverse
 class TestV2Urls(base.BaseCase):
     def setUp(self):
         self.c = Client()
-        self.poa_id = '10.7554/eLife.16950'
-        self.vor_id = 
+        self.poa_id = '16950'
+        self.vor_id = self.poa_id
 
     def tearDown(self):
         pass
@@ -29,8 +29,8 @@ class TestV2Urls(base.BaseCase):
         expected_content_type = 'application/vnd.elife.article-history+json;version=1'
         self.assertEqual(resp.content_type, expected_content_type)
 
-    def test_article_version_list(self):
-        resp = self.c.get(reverse('v2:article-version'kwargs={'id': self.poa_id, 'version': 1}))
+    def test_article_version(self):
+        resp = self.c.get(reverse('v2:article-version', kwargs={'id': self.poa_id, 'version': 1}))
         self.assertEqual(resp.status_code, 200)
         expected_content_type = 'application/vnd.elife.article-poa+json;version=1'
         self.assertEqual(resp.content_type, expected_content_type)
