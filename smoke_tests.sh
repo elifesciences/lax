1f367ec979cc88df6dbe12b3c7e39e44aa708e72
Giorgio Sironi <g.sironi@elifesciences.org>
2016-08-26 16:16:06 +0100

    Smoke tests should use HTTPS, that all AWS instances should offer

--- smoke_tests.sh
+++ smoke_tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -ex
 
-[ $(curl --write-out %{http_code} --silent --output /dev/null $(hostname)/) == 200 ]
+[ $(curl --write-out %{http_code} --silent --output /dev/null https://$(hostname)/) == 200 ]
