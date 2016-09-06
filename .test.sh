a64ea0ebe5cffe9f95a6b05f870f86d0db577b81
Luke Skibinski <l.skibinski@elifesciences.org>
2016-09-06 14:49:29 +0100

    added ability to run individual test modules.

--- .test.sh
+++ .test.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
 
-set -e
+set -e # everything must pass
+
+args="$@"
+module="src"
+print_coverage=1
+if [ ! -z "$args" ]; then
+    module="$args"
+    print_coverage=0
+fi
 
 # called by test.sh
 rm -f build/junit.xml
-coverage run --source='src/' --omit='*/tests/*,*/migrations/*' src/manage.py test src/ --no-input
+coverage run --source='src/' --omit='*/tests/*,*/migrations/*' src/manage.py test "$module" --no-input
 echo "* passed tests"
 
-coverage report
-
-# is only run if tests pass
-covered=$(coverage report | grep TOTAL | awk '{print $4}' | sed 's/%//')
-if [ $covered -lt 65 ]; then
-    echo
-    echo "FAILED this project requires at least 65% coverage, got $covered"
-    echo
-    exit 1
+# run coverage test
+# only report coverage if we're running a complete set of tests
+if [ $print_coverage -eq 1 ]; then
+    coverage report
+    # is only run if tests pass
+    covered=$(coverage report | grep TOTAL | awk '{print $4}' | sed 's/%//')
+    if [ $covered -lt 65 ]; then
+        echo
+        echo "FAILED this project requires at least 65% coverage, got $covered"
+        echo
+        exit 1
+    fi
 fi
 
