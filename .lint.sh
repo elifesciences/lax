983c693cdf9e6a178cc615cae31f2259478798c9
Luke Skibinski <l.skibinski@elifesciences.org>
2016-09-08 12:53:04 +0100

    added pyflakes to linting step

--- .lint.sh
+++ .lint.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 set -e
+echo "* calling pylint"
 pylint -E ./src/publisher/** --load-plugins=pylint_django --disable=E1103
+echo "* calling pyflakes"
+pyflakes ./src/
 echo "* passed linting"
