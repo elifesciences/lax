77fc5a74abd9e56b7bd99543fd27b953b6850d7d
Luke Skibinski <l.skibinski@elifesciences.org>
2016-09-08 19:11:17 +0100

    pyflakes is now called first when linting because it's (much) faster

--- .lint.sh
+++ .lint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
-echo "* calling pylint"
-pylint -E ./src/publisher/** --load-plugins=pylint_django --disable=E1103
 echo "* calling pyflakes"
 pyflakes ./src/
+echo "* calling pylint"
+pylint -E ./src/publisher/** --load-plugins=pylint_django --disable=E1103
 echo "* passed linting"
