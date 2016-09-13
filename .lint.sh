c19d00428732f569398b6314ad63d148eaa5313f
Luke Skibinski <l.skibinski@elifesciences.org>
2016-09-13 11:38:38 +0100

    upgraded pylint, removed any pyc files before calling linters. send pylint stderr to /dev/null - it prints a deprecation warning without a newline

--- .lint.sh
+++ .lint.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 set -e
+
+# remove any old compiled python files
+# pylint likes to lint them
+find src/ -name '*.pyc' -delete
+
 echo "* calling pyflakes"
 pyflakes ./src/
 echo "* calling pylint"
-pylint -E ./src/publisher/** --load-plugins=pylint_django --disable=E1103
+pylint -E ./src/publisher/** --load-plugins=pylint_django --disable=E1103 2> /dev/null
 echo "* passed linting"
