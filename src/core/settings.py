332c0375811b7277119c41e7de5fa87aaf89a124
Luke Skibinski <l.skibinski@elifesciences.org>
2016-10-31 15:55:41 +0000

    fixed issue with html rendering of json results. upgraded swagger and rest framework.

--- src/core/settings.py
+++ src/core/settings.py
@@ -151,12 +151,12 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [],
     #'DEFAULT_CONTENT_NEGOTIATION_CLASS': 'publisher.negotiation.eLifeContentNegotiation',
     'DEFAULT_RENDERER_CLASSES': (
-        'rest_framework.renderers.JSONRenderer',
         'publisher.negotiation.ArticleListVersion1',
         'publisher.negotiation.POAArticleVersion1',
         'publisher.negotiation.VORArticleVersion1',
         'publisher.negotiation.ArticleHistoryVersion1',
-        'rest_framework.renderers.BrowsableAPIRenderer',
+        'rest_framework.renderers.JSONRenderer',
+        #'rest_framework.renderers.BrowsableAPIRenderer',
     )
 }
 
