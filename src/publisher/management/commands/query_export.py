995e3a1c8e2f604c83646940a01efa696a646c92
Luke Skibinski <l.skibinski@elifesciences.org>
2016-11-08 14:35:11 +0000

    added tests for the query upload cli

--- src/publisher/management/commands/query_export.py
+++ src/publisher/management/commands/query_export.py
@@ -1,3 +1,7 @@
+import sys
+from publisher import utils
+from functools import partial
+from slugify import slugify
 from django.core.management.base import BaseCommand
 from django.conf import settings
 from datetime import date
@@ -8,38 +12,58 @@ import tinys3
 
 LOG = logging.getLogger(__name__)
 
-def _upload(key, data):
-    credentials = botocore.session.get_session().get_credentials()
-    access_key = credentials.access_key
-    secret_key = credentials.secret_key
-    conn = tinys3.Connection(access_key, secret_key, default_bucket=settings.EXPLORER_S3_BUCKET)
-    return conn.upload(key, data)  # expects a file-like object
-
-def snapshot_query(query_id):
-    LOG.info("Starting snapshot for query %s..." % query_id)
-    q = models.Query.objects.get(pk=query_id)
-    exporter = exporters.get_exporter_class('csv')(q)
-    k = 'query-%s.snap-%s.csv' % (q.id, date.today().strftime('%Y%m%d-%H:%M:%S'))
-    LOG.info("Uploading snapshot for query %s as %s..." % (query_id, k))
-    resp = _upload(k, exporter.get_file_output())
-    LOG.info("Done uploading snapshot for query %s. URL: %s" % (query_id, resp.url))
-
 class Command(BaseCommand):
     help = 'for snapshotting queries and uploading to s3 without celery'
 
     def add_arguments(self, parser):
         parser.add_argument('--query-id', dest='qid', type=int, required=False)
+        parser.add_argument('--skip-upload', action='store_false', dest='upload')
+        parser.add_argument('--timestamp-filenames', dest='timestamp_fname', action='store_true')
+
+    def _upload(self, key, data):
+        credentials = botocore.session.get_session().get_credentials()
+        access_key = credentials.access_key
+        secret_key = credentials.secret_key
+        conn = tinys3.Connection(access_key, secret_key, default_bucket=settings.EXPLORER_S3_BUCKET)
+        return conn.upload(key, data)  # expects a file-like object
+
+    def snapshot_query(self, query_id, upload=True, timestamp_fname=False):
+        q = models.Query.objects.get(pk=query_id)
+        exporter = exporters.get_exporter_class('csv')(q)
+        safe_title = slugify(q.title)
+        fname = 'query%s--%s.csv' % (q.id, safe_title) # ll: query1--dummy-query.csv
+        if timestamp_fname:
+            # ll: query1--dummy-query--20160131-23:59:59
+            fname = 'query%s--%s--%s.csv' % (q.id, safe_title, date.today().strftime('%Y%m%d-%H:%M:%S'))
+        if upload:
+            LOG.info("uploading snapshot: %s", fname)
+            self._upload(fname, exporter.get_file_output())
+            LOG.info("completed upload of snapshot: %s", fname)
+            self.echo('%s uploaded' % fname)
+        self.echo(fname)
+
+    def echo(self, x):
+        self.stdout.write(str(x))
+        self.stdout.flush()
 
     def handle(self, *args, **options):
-        qid = options['qid']
+        try:
+            qid = options['qid']
+            qid_list = []
+            if qid:
+                qid_list = [qid]
+            else:
+                qid_list = models.Query.objects.all().values_list('id', flat=True)
 
-        qid_list = []
-        if qid:
-            qid_list = [qid]
-        else:
-            qid_list = models.Query.objects.all().values('id')
+            if not qid_list:
+                LOG.info("no query objects found, nothing to upload")
+            else:
+                fnargs = utils.subdict(options, ['upload', 'timestamp_fname'])
+                map(partial(self.snapshot_query, **fnargs), qid_list)
 
-        if not qid_list:
-            LOG.info("no query objects found, nothing to upload")
+        except Exception as err:
+            LOG.exception(err)
+            self.echo(str(err))
+            sys.exit(1)
 
-        map(snapshot_query, qid_list)
+        sys.exit(0)
---
+++ src/publisher/tests/test_query_export.py
@@ -0,0 +1,50 @@
+import re
+from explorer import models
+from django.core.management import call_command
+from .base import BaseCase
+from StringIO import StringIO
+
+class CLI(BaseCase):
+    def setUp(self):
+        self.nom = 'query_export'
+        q = models.Query(**{
+            'title': 'dummy-query',
+            'sql': 'select count(*) from publisher_article;'
+        })
+        q.save()
+        self.q = models.Query.objects.first()
+
+    def call_command(self, *args, **kwargs):
+        stdout = StringIO()
+        try:
+            kwargs['stdout'] = stdout
+            call_command(*args, **kwargs)
+        except SystemExit as err:
+            return err.code, stdout
+        self.fail("management command should always throw a systemexit()")
+
+    def test_export_all_from_cli(self):
+        args = [self.nom, '--skip-upload']
+        errcode, stdout = self.call_command(*args)
+        self.assertEqual(errcode, 0)
+
+    def test_export_specific_from_cli(self):
+        args = [self.nom, '--skip-upload', '--query-id', str(self.q.id)]
+        errcode, stdout = self.call_command(*args)
+        self.assertEqual(errcode, 0)
+        cregex = re.compile('query.--dummy-query.csv')
+        self.assertNotEqual(cregex.match(stdout.getvalue()), None)
+
+    def test_export_specific_from_cli_timestamp_fname(self):
+        args = [self.nom, '--skip-upload', '--query-id', str(self.q.id), '--timestamp-filename']
+        errcode, stdout = self.call_command(*args)
+        cregex = re.compile(r'query.--dummy-query--.{8}-.{8}\.csv')
+        self.assertEqual(errcode, 0)
+        self.assertNotEqual(cregex.match(stdout.getvalue()), None)
+
+    def test_export_empty_queryset(self):
+        "no queries? no worries."
+        args = [self.nom, '--skip-upload']
+        models.Query.objects.all().delete()
+        errcode, stdout = self.call_command(*args)
+        self.assertEqual(errcode, 0)
