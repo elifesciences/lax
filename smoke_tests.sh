203cadd01276e29adfad693e96fcfdddc7dbd07f
Giorgio Sironi <g.sironi@elifesciences.org>
2016-11-29 11:13:25 +0000

    Adding both 200 for reports and 301 for protected pages

--- smoke_tests.sh
+++ smoke_tests.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 set -ex
 
-[ $(curl --write-out %{http_code} --silent --output /dev/null https://$(hostname)/) == 200 ]
-[ $(curl --write-out %{http_code} --silent --output /dev/null https://$(hostname)/api/v2/articles) == 200 ]
+for path in / /api/v2/articles /rss/articles/poa+vor/last-28-days/ /reports/paw/recent.xml; do
+    [ $(curl --write-out %{http_code} --silent --output /dev/null https://$(hostname)$path) == 200 ]
+done
+
+for path in /admin /explorer; do
+    [ $(curl --write-out %{http_code} --silent --output /dev/null https://$(hostname)$path) == 301 ]
+done
