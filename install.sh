88628bd0e0bf8dd53b544f06d5ea2012725a2163
Luke Skibinski <l.skibinski@elifesciences.org>
2017-01-17 11:55:44 +0000

    install.sh now rebuilds venv if precise version of python not found

--- install.sh
+++ install.sh
@@ -3,8 +3,22 @@ set -e # everything must succeed.
 
 . download-api-raml.sh
 
-if [ ! -d venv ]; then
-    virtualenv --python=`which python2` venv
+# use the latest version of python2 we can find.
+
+# ll: /usr/bin/python2.7
+maxpy=$(which /usr/bin/python2* | grep -E '[0-9]$' | sort -r | head -n 1)
+
+# ll: python2.7
+# http://stackoverflow.com/questions/2664740/extract-file-basename-without-path-and-extension-in-bash
+py=${maxpy##*/} # magic
+
+# check for exact version of python
+if [ ! -e "venv/bin/$py" ]; then
+    echo "rebuilding venv"
+    rm -rf venv
+    virtualenv --python="$maxpy" venv
+else
+    echo "found venv/bin/$py"
 fi
 source venv/bin/activate
 if [ ! -e app.cfg ]; then
