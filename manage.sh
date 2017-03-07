dedbf3a4e7f186c36de2e7fa3075b1d5e35c36e7
Luke Skibinski <l.skibinski@elifesciences.org>
2017-03-08 09:31:53 +1030

    added a flag `--skip-install` that will skip calling install.sh

--- manage.sh
+++ manage.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
+set -exv
+
 # @description convenience wrapper around Django's runserver command
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
 # this starts to reinstall everything in production everytime an article is ingested:
-#source install.sh > /dev/null
+
+skip_install=$1
+if test "$skip_install" != "--skip-install"; then
+    echo "installing"
+    source install.sh > /dev/null
+else
+    echo "not installing"
+    shift # like 'pop' but for positional args
+fi
 
 source venv/bin/activate
 ./src/manage.py $@
