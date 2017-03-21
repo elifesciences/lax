e82d28c28903869326f20dd6a8e57b95fe0c5850
Luke Skibinski <l.skibinski@elifesciences.org>
2017-03-21 14:45:37 +1030

    added caching headers to all responses

--- src/core/middleware.py
+++ src/core/middleware.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from netaddr import IPAddress, IPNetwork
 import logging
+from django.utils.deprecation import MiddlewareMixin
 
 LOG = logging.getLogger(__name__)
 
@@ -18,7 +19,7 @@ def strip_auth_headers(request, authenticated=False):
             del request.META[header]
     set_authenticated(request, authenticated)
 
-class KongAuthentication(object):
+class KongAuthentication(MiddlewareMixin):
     def process_response(self, request, response):
         hdr = settings.KONG_AUTH_HEADER
         response[hdr] = request.META.get(hdr, False)
@@ -59,3 +60,37 @@ class KongAuthentication(object):
 
         strip_auth_headers(request, authenticated=True)
         LOG.debug("authenticated!")
+
+#
+#
+#
+
+from django.views.decorators.cache import patch_cache_control
+from django.utils.cache import patch_vary_headers
+
+class DownstreamCaching(object):
+    def __init__(self, get_response):
+        self.get_response = get_response
+    
+    def __call__(self, request):
+        public_headers = {
+            'public': True,
+            'max-age': 60 * 5, # 5 minutes, 300 seconds
+            'stale-while-revalidate': 60 * 5, # 5 minutes, 300 seconds
+            'stale-if-error': (60 * 60) * 24, # 1 day, 86400 seconds
+        }
+        private_headers = {
+            'private': True,
+            'max-age': 0, # seconds
+            'must-revalidate': True,
+        }
+        
+        authenticated = request.META[settings.KONG_AUTH_HEADER]
+        headers = public_headers if not authenticated else private_headers
+        
+        response = self.get_response(request)
+        
+        patch_cache_control(response, **headers)
+        patch_vary_headers(response, ['Accept'])
+
+        return response
--- src/core/tests.py
+++ src/core/tests.py
@@ -43,3 +43,31 @@ class KongAuthMiddleware(TestCase):
         resp = self.c.get('/', **self.extra)
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(resp[settings.KONG_AUTH_HEADER], 'False')
+
+class DownstreamCachine(TestCase):
+    def setUp(self):
+        self.c = Client()
+        self.url = '/' # we could hit more urls but it's applied application-wide
+
+    def tearDown(self):
+        pass
+
+    def test_cache_headers_in_response(self):        
+        expected_headers = [
+            'vary',
+            'etag',
+            'cache-control'
+        ]
+        resp = self.c.get(self.url)
+        for header in expected_headers:
+            self.assertTrue(resp.has_header(header), "header %r not found in response" % header)
+
+    def test_cache_headers_not_in_response(self):
+        cases = [
+            'expires',
+            'last-modified',
+            'prama'
+        ]
+        resp = self.c.get(self.url)
+        for header in cases:
+            self.assertFalse(resp.has_header(header), "header %r present in response" % header)
