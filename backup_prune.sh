d36d35df60e28e7240d43670dcaee2b95d1da50d
Giorgio Sironi <g.sironi@elifesciences.org>
2017-03-23 10:05:31 +0000

    Updating backup_prune.sh for corner cases

--- backup_prune.sh
+++ backup_prune.sh
@@ -2,7 +2,10 @@
 set -e
 
 keep=${1:-3}
-line_to_start_from=$(echo ${keep}+1 | bc)
+line_to_start_from=$(echo "${keep}"+1 | bc)
 
 files_to_delete=$(find /tmp -name 'lax-db-*' -printf "%T %p\n" | sort -r | tail -n "+$line_to_start_from" | awk '{ print $2 }')
-rm $files_to_delete
+if [ -n "${files_to_delete}" ]; then
+    rm "${files_to_delete}"
+fi
+
