1f7864ca444aee3025c8be1841b925f3d3a8c5f3
Luke Skibinski <l.skibinski@elifesciences.org>
2017-04-03 10:33:24 +0930

    fixed missing leading arrows in urls. this made certains urls possible when they shouldn't have been.

--- src/publisher/api_v1_urls.py
+++ src/publisher/api_v1_urls.py
@@ -4,16 +4,16 @@ from . import api_v1_views as views
 urlpatterns = [
     #url(r'corpus/info/$', views.corpus_info, name='api-corpus-info'),
 
-    url(r'article/create/$', views.create_article, name='api-create-article'),
-    url(r'article/update/$', views.update_article, name='api-update-article'),
-    url(r'article/create-update/$', views.import_article, name='api-create-update-article'),
+    url(r'^article/create/$', views.create_article, name='api-create-article'),
+    url(r'^article/update/$', views.update_article, name='api-update-article'),
+    url(r'^article/create-update/$', views.import_article, name='api-create-update-article'),
 
     # all versions of an article
-    url(r'article/(?P<doi>[\.\w]+\/[\.\w]+)/version/$', views.get_article_versions, name='api-article-versions'),
+    url(r'^article/(?P<doi>[\.\w]+\/[\.\w]+)/version/$', views.get_article_versions, name='api-article-versions'),
 
     # specific version of a specific article
-    url(r'article/(?P<doi>[\.\w]+\/[\.\w]+)/version/(?P<version>\d{0,3})/$', views.get_article, name='api-article-version'),
+    url(r'^article/(?P<doi>[\.\w]+\/[\.\w]+)/version/(?P<version>\d{0,3})/$', views.get_article, name='api-article-version'),
 
     # latest version of a specific article
-    url(r'article/(?P<doi>[\.\w]+\/[\.\w]+)/$', views.get_article, name='api-article'),
+    url(r'^article/(?P<doi>[\.\w]+\/[\.\w]+)/$', views.get_article, name='api-article'),
 ]
--- src/publisher/api_v2_urls.py
+++ src/publisher/api_v2_urls.py
@@ -3,12 +3,12 @@ from . import api_v2_views as views
 
 urlpatterns = [
     url(r'^ping$', views.ping, name='ping'),
-    url(r'articles$', views.article_list, name='article-list'),
-    url(r'articles/(?P<id>\d+)$', views.article, name='article'),
-    url(r'articles/(?P<id>\d+)/versions$', views.article_version_list, name='article-version-list'),
-    url(r'articles/(?P<id>\d+)/versions/(?P<version>\d+)$', views.article_version, name='article-version'),
-    url(r'articles/(?P<id>\d+)/related$', views.article_related, name='article-relations'),
+    url(r'^articles$', views.article_list, name='article-list'),
+    url(r'^articles/(?P<id>\d+)$', views.article, name='article'),
+    url(r'^articles/(?P<id>\d+)/versions$', views.article_version_list, name='article-version-list'),
+    url(r'^articles/(?P<id>\d+)/versions/(?P<version>\d+)$', views.article_version, name='article-version'),
+    url(r'^articles/(?P<id>\d+)/related$', views.article_related, name='article-relations'),
 
     # not part of Public API
-    url(r'articles/(?P<art_id>\d+)/fragments/(?P<fragment_id>[\-\w]{3,25})$', views.article_fragment, name='article-fragment'),
+    url(r'^articles/(?P<art_id>\d+)/fragments/(?P<fragment_id>[\-\w]{3,25})$', views.article_fragment, name='article-fragment'),
 ]
