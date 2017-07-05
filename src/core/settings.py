722b17da361e0c905ffe0b8e37f5e122361b6394
Luke Skibinski <l.skibinski@elifesciences.org>
2017-07-05 16:04:30 +0930

    fixed up the log dir. it's now relative to the project dir or /var/log/, not whatever directory you happen to be in

--- src/core/settings.py
+++ src/core/settings.py
@@ -49,8 +49,12 @@ SECRET_KEY = cfg('general.secret-key')
 DEBUG = cfg('general.debug')
 assert isinstance(DEBUG, bool), "'debug' must be either True or False as a boolean, not %r" % (DEBUG, )
 
-DEV, TEST, PROD = 'dev', 'test', 'prod'
+DEV, VAGRANT, TEST, PROD = 'dev', 'vagrant', 'test', 'prod'
 ENV = cfg('general.env', DEV)
+
+# temporary until the 'vagrant' env is more formalised
+if os.path.exists('/vagrant'):
+    ENV = VAGRANT
 
 ALLOWED_HOSTS = cfg('general.allowed-hosts', '').split(',')
 
@@ -263,7 +267,7 @@ LOG_NAME = '%s.log' % PROJECT_NAME # ll: lax.log
 
 INGESTION_LOG_NAME = 'ingestion-%s.log' % PROJECT_NAME
 
-LOG_DIR = '/var/log/' if ENV != DEV else './'
+LOG_DIR = PROJECT_DIR if ENV == DEV else '/var/log/'
 LOG_FILE = join(LOG_DIR, LOG_NAME) # ll: /var/log/lax.log
 INGESTION_LOG_FILE = join(LOG_DIR, INGESTION_LOG_NAME) # ll: /var/log/lax.log
 
