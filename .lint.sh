62c8d12db72e94a9817326c6863df05cbdd9def9
Luke <lsh-0@users.noreply.github.com>
2018-10-29 11:53:12 +1030

    improved linting

--- .lint.sh
+++ .lint.sh
@@ -3,21 +3,23 @@ set -e
 
 echo "[-] .lint.sh"
 
+# remove any old compiled python files
 # pylint likes to lint them
 find src/ -name '*.py[c|~]' -delete
 find src/ -regex "\(.*__pycache__.*\|*.py[co]\)" -delete
 
 echo "pyflakes"
 pyflakes ./src/
-# disabled until pylint supports Python 3.6
-# https://github.com/PyCQA/pylint/issues/1113
 
 echo "pylint"
-pylint -E ./src/publisher/** --load-plugins=pylint_django --disable=E1103 2> /dev/null
+# E1103 - a variable is accessed for a nonexistent member, but astng was not able to interpret all possible types of this variable.
+pylint -E ./src/publisher/** --load-plugins=pylint_django \
+    --disable=E1103 2> /dev/null
 # specific warnings we're interested in, comma separated with no spaces
 # presence of these warnings are a failure
-pylint ./src/publisher/** --load-plugins=pylint_django --disable=all --reports=n --score=n \
-    --enable=redefined-builtin
+pylint ./src/publisher/** --load-plugins=pylint_django \
+    --disable=all --reports=n --score=n \
+    --enable=redefined-builtin,pointless-string-statement,no-else-return,redefined-outer-name
 
 echo "scrubbing"
 . .scrub.sh 2> /dev/null
