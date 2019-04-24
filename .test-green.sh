e6b971b83b30f7ac73518ebe94429e43547f40b2
Luke <lsh-0@users.noreply.github.com>
2019-04-24 11:10:38 +0930

    switches project_tests.sh to use .test-green.sh rather than .test.sh

--- .test-green.sh
+++ .test-green.sh
@@ -12,6 +12,12 @@ if [ ! -z "$args" ]; then
     module="src.$args"
 fi
 
+# remove any old compiled python files
+find src/ -name '*.pyc' -delete
+
+# called by test.sh
+rm -f build/junit.xml
+
 GREEN_CONFIG=.green LAX_MULTIPROCESSING=1 ./src/manage.py test "$module" \
     --testrunner=green.djangorunner.DjangoRunner \
     --no-input \
