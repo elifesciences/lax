4a6c3f24ebec93bf1714d7854af19e7dbabae9e4
Luke <lsh-0@users.noreply.github.com>
2020-09-08 16:41:35 +0930

    adds Pipfile, Pipfile.lock and update-dependencies.sh script

--- canary.sh
+++ canary.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
-
-# everything must pass
 set -e
+. update-dependencies.sh 
+. project_tests.sh
 
-# reload the virtualenv
-rm -rf venv/
-. mkvenv.sh
-source venv/bin/activate
-pip install -r requirements.txt
-
-# upgrade all deps to latest version
-pip install pip-review
-pip-review --pre # preview the upgrades
-echo "[any key to continue ...]"
-read -p "$*"
-pip-review --auto --pre # update everything
-pip freeze > new-requirements.txt
-
-echo
-echo "wrote 'new-requirements.txt'"
