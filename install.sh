a4e4e4162bdf27418d5cad016f7cb10ab390ce11
Luke <lsh-0@users.noreply.github.com>
2021-07-28 19:04:24 +0930

    requirements, removes django-restframework from dependencies

--- install.sh
+++ install.sh
@@ -13,7 +13,8 @@ if [ ! -e app.cfg ]; then
     ln -s elife.cfg app.cfg
 fi
 
-pip install -r requirements.txt --no-cache-dir
+pip install pip wheel --upgrade
+pip install -r requirements.txt
 NEW_RELIC_EXTENSIONS=false pip install --no-binary :all: newrelic==2.82.0.62
 
 python src/manage.py migrate --no-input
