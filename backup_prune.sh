64f39e6157bf9ef12a2e782e87d241dcacd66ef4
Luke <lsh-0@users.noreply.github.com>
2022-03-09 17:42:11 +1030

    backup.sh, switched backup from a django thing to UBR. backup_prune.sh now prunes the log file generated by UBR

--- backup_prune.sh
+++ backup_prune.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+# lsh@2022-03-09: delete any backup files.
+rm /tmp/lax-db-jenkins-prod-lax-*.json.gz
+
+# lsh@2022-03-09: this now prunes the ubr backup log created in backup.sh
 keep=${1:-3}
 line_to_start_from=$(echo "${keep}"+1 | bc)
 
