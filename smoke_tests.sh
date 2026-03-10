becbbec588a28a45b5b7336ec9345c92e118c8bb
LinaKind <l.kinduryte@elifesciences.org>
2026-03-10 13:26:55 +0200

    Exit with non zero when curl calls fail
    https://github.com/elifesciences/issues/issues/9497

--- smoke_tests.sh
+++ smoke_tests.sh
@@ -17,6 +17,8 @@ request () {
         test "$expected" = "$actual" && break
         sleep $retry_delay
     done
+
+    test "$expected" = "$actual" || exit 1
 }
 
 for path in / /api/v2/articles; do
